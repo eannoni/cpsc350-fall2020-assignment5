5da9c3b3d19360fdbb133c4543d340ddb34b15a8
Eli Annoni <eliannoni1@gmail.com>
2020-11-24 18:12:18 -0800

    main is fully functional with using Student objects and BST.

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include "BinarySearchTree.h"
+#include "Person.h"
 
 #include <iostream>
 
@@ -6,27 +7,32 @@ using namespace std;
 
 int main(int argc, char **argv) {
 
-    BinarySearchTree<int> *myBST = new BinarySearchTree<int>();
+    BinarySearchTree<Person> *myBST = new BinarySearchTree<Person>();
 
-    myBST->insertNode(myInt1);
-    myBST->insertNode(myInt2);
-    myBST->insertNode(myInt3);
+    Person *myPerson1 = new Person(4, "John Smith", "Sophomore");
+    Person* myPerson2 = new Person(5, "Jane Doe", "Junior");
+    Person *myPerson3 = new Person(1, "ABC", "Freshman");
+    Person *myPerson4 = new Person(6, "DEF", "Senior");
+    Person *myPerson5 = new Person(2, "GHI", "Graduate");
+    Person *myPerson6 = new Person(3, "JKL", "Prospective");
 
-    myBST->printEntireTree();
-    cout << "size: " << myBST->getSize() << endl;
-    cout << "max: " << myBST->getMax() << endl;
-    cout << "min: " << myBST->getMin() << endl;
-
-    myBST->insertNode(myInt4);
-    myBST->insertNode(myInt5);
-    myBST->insertNode(myInt6);
+    myBST->insertNode(myPerson1);
+    myBST->insertNode(myPerson2);
+    myBST->insertNode(myPerson3);
 
     myBST->printEntireTree();
     cout << "size: " << myBST->getSize() << endl;
-    cout << "max: " << myBST->getMax() << endl;
-    cout << "min: " << myBST->getMin() << endl;
+    cout << "max: " << *myBST->getMax() << endl;
+    cout << "min: " << *myBST->getMin() << endl;
 
+    myBST->insertNode(myPerson4);
+    myBST->insertNode(myPerson5);
+    myBST->insertNode(myPerson6);
 
+    myBST->printEntireTree();
+    cout << "size: " << myBST->getSize() << endl;
+    cout << "max: " << *myBST->getMax() << endl;
+    cout << "min: " << *myBST->getMin() << endl;
 
     delete myBST;
 
