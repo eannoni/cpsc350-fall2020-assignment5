0104e9e3d170e889f924ed36a4412a0554790a3a
eannoni <annoni@chapman.edu>
2020-11-26 00:08:16 +0000

    updated main to test functionality of new classes

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include "BinarySearchTree.h"
-#include "Person.h"
+#include "Student.h"
+#include "Faculty.h"
 
 #include <iostream>
 
@@ -9,12 +10,17 @@ int main(int argc, char **argv) {
 
     BinarySearchTree<Person> *myBST = new BinarySearchTree<Person>();
 
-    Person *myPerson1 = new Person(4, "John Smith", "Sophomore");
-    Person* myPerson2 = new Person(5, "Jane Doe", "Junior");
+    vector<int>* adviseeList = new vector<int>();
+    adviseeList->push_back(5);
+    adviseeList->push_back(1);
+    adviseeList->push_back(9);
+
+    Student *myPerson1 = new Student(4, "John Smith", "Sophomore", "Computer Science", 4.0, 23);
+    Faculty* myPerson2 = new Faculty(5, "Jane Doe", "Professor", "Engineering", adviseeList);
     Person *myPerson3 = new Person(1, "ABC", "Freshman");
     Person *myPerson4 = new Person(6, "DEF", "Senior");
     Person *myPerson5 = new Person(2, "GHI", "Graduate");
-    Person *myPerson6 = new Person(3, "JKL", "Prospective");
+    Faculty *myPerson6 = new Faculty(*myPerson2);
 
     myBST->insertNode(myPerson1);
     myBST->insertNode(myPerson2);
